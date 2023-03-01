c49e0fe2d0c3dbdde2b860df7356754ddbbe62e1
Anudeep K K <anudeep2003@gmail.com>
2023-03-01 17:45:56 +0530

    Different Components  are added by separating the Header and Footer

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import reportWebVitals from './reportWebVitals';
 
 import HomePage from './pages/HomePage';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HomePage />
+
   </React.StrictMode>
 );
 
