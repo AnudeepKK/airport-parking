fca140c4e818974ae9011f017cecb904491854d3
Anudeep K K <anudeep2003@gmail.com>
2023-03-15 19:19:18 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './style.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Layout from './component/Layout';
-import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import AirportAvailability from './pages/AirportAvailability';
+import HomePage from './pages/Home';
 function App() {
   return (
    <BrowserRouter>
